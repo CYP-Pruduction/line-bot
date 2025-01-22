20f506b0d79fbdf05b9d2f98fdaa1d404969ca1f
Teddy Chiu (邱信融) <teddy.chiu@cypress.com.tw>
2025-01-22 10:38:46 +0800

    更新 line_app.py

--- line_app.py
+++ line_app.py
@@ -522,5 +522,7 @@ def init_db():
         print("Database initialized")
 
 if __name__ == "__main__":
+    with app.app_context():
+       init_db()
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
